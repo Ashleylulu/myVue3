40a4e1bf3bfa0470d0f1d739f69bcb8b37804f98
lumy <412005874@qq.com>
2021-05-21 11:06:14 +0800

    dev

--- src/components/Comps.vue
+++ src/components/Comps.vue
@@ -2,32 +2,21 @@
     <div>
         <injection></injection>
         <div>
-            <ul>
-                <li v-for="item in book">{{item}}</li>
-            </ul>
+            {{book}}
         </div>
     </div>
 </template>
 <script>
-import {ref,toRefs,computed,watch,onMounted , reactive , inject, h} from 'vue' // 实现带ref的响应式变量
+import {inject} from 'vue' // 实现带ref的响应式变量
 import injection from '../components/Injection'
 export default {
     components:{injection},
     setup(){
         let book = inject('book');
-        let state = reactive({count:0})
-        const increat = () => {
-            state.count++
-            console.log(state)
-        }
         return {
             book
         }
     },
-    render(){
-        return h('div')
-    }
-
 }
 </script>
 <style scoped>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,70 +1,20 @@
 <template>
     <div>
-        <comps v-styc="co"></comps>
-        <input type="range" min="0" max="300" ref="int" v-model="pinval">
-        <div ref="box" v-pin:[dir] = 'pinval'>我在移动</div>
-        <div v-sty="{color:'red',text:'hello'}"></div>
+        <comps></comps>
     </div>
 </template>
 <script>
     import comps from '../components/Comps'
-    import {toRefs,toRef,provide,reactive,ref,onMounted,watch} from 'vue'
-    const homeMixin = {
-        data(){
-            return {
-                box:'box',
-                dir:'right',
-                pinval:200,
-                co:"pink"
-            }
-        }
-    }
+    import {provide,ref} from 'vue'
+
     export default {
         components:{comps},
-        directives:{
-           focus:{
-               mounted(el){
-                   el.focus()
-               }
-           },
-            //函数表达式形式
-            pin(el,binding) {
-                el.style.position = 'fixed';
-                let dec = binding.arg || 'top';
-                el.style[dec] = binding.value + 'px'
-            },
-            //对象字面量
-            sty(el,binding) {
-               let val = binding.value;
-               el.style.color = val.color;
-               el.innerText = val.text;
-            },
-            //在组件中使用
-            styc(el,binding) {
-               el.style.color = binding.value;
-            }
-            //在组件中使用
-
-        },
-        setup(props,context){
-            provide('title',ref('首页'));
-            provide('book',reactive([
-                'kuangrenriji','nahan'
-            ]))
-
-            let box = ref(null);
-            let int = ref('123');
-            onMounted(() => {
-                console.log(box.value)//<div>xinqingnian</div>
-                console.log(int.value)//<input type="text">
-            })
-            return {
-                box,
-                int
-            }
-        },
-        mixins:[homeMixin],
-        created(){
+        setup(){
+            let book = ref('yao');
+            let title = ref('首页');
+            provide('book',book)
+            provide('title',title)
         }
+
     }
 </script>
